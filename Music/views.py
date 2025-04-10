310c4972666617d55a5a76ff8393a865716b14a4
Darién Julián Ramírez <ianshalaga@gmail.com>
2025-04-10 23:28:37 +0200

    First templates created

--- Music/views.py
+++ Music/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def song(request):
+    context = {
+        'title': 'Mi primera pagina',
+        "content": "Cuerpo de la página"
+    }
+    return render(request, 'Music/song.html', context)
--- MyApp/views.py
+++ MyApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    context = {'title': 'Mi primera pagina'}
+    return render(request, 'MyApp/index.html', context=context)
